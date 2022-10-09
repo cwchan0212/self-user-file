f6838d0b6b9ff4f918f045a1eb42618e41c8f282
C. W. Chan <cwchan0212@gmail.com>
2022-10-09 17:17:56 +0100

    add CRUD not alert message

--- server.js
+++ server.js
@@ -5,18 +5,28 @@ const cookieParser = require("cookie-parser");
 const flash = require("connect-flash");
 const app = express();
 
+
+
 require("dotenv").config();
 const port = process.env.PORT || 3000;
 
-// Load layouts/main.ejs
 app.use(express.urlencoded( { extended: true } ));
 app.use(express.static('public'));
 app.use(expressLayouts);
+
+app.use(cookieParser('UserDataSecure'));
+app.use(session({
+    secret: 'secret key',
+    resave: false,
+    saveUninitialized: false
+  }));
+
+app.use(flash());
+
 app.set("layout", "./layouts/main");
 app.set("view engine", "ejs");
 
 const routes = require("./server/routes/userRoutes.js");
 app.use("/", routes);
-
 
 app.listen(port, () => console.log(`Listening to port ${port}...`));
--- server/controllers/userController.js
+++ server/controllers/userController.js
@@ -1,18 +1,10 @@
 require("../models/database");
 const { remove } = require("../models/User");
 const User = require("../models/User");
+
 exports.homepage = async (req, res) => {
     try {
         const users = await User.find({}).sort({ _id: -1 });
-
-        // console.log(users.length);
-        // res.setHeader("Content-Type", "application/json");
-        // res.json(users);
-        // users.forEach(function (user, index) {
-        //   console.log(user.dob.date, user.name.first, user.picture.thumbnail, user.email, user.phone, user.location.country);
-        //     // console.log(user.location.country)
-        // });
-
         res.render("index", { title: "User Profile Management - CRUD", users });
     } catch (error) {
         res.status(500).send({ message: error.message || "Error Occurred" });
@@ -21,46 +13,92 @@ exports.homepage = async (req, res) => {
 
 exports.modified = async (req, res) => {
     try {
-        // let mode = "save";
-        // let id = req.body.submit-save ? req.body.submit-save: req.body.submit-remove;
-
         let id = req.params.id;
         let action = req.body.save ? "update" : "remove";
+        console.log(req.body);
 
         if (id) {
             const userData = {
-                title: req.body["title-" + id],
-                first: req.body["first-" + id],
-                last: req.body["last-" + id],
+                name: {
+                    title: req.body["title-" + id],
+                    first: req.body["first-" + id],
+                    last: req.body["last-" + id],
+                },
+
                 gender: req.body["gender-" + id],
-                dob: req.body["dob-" + id],
-                country: req.body["country-" + id],
+                dob: {
+                    date: new Date(req.body["dob-" + id]),
+                    age:
+                        new Date().getFullYear() -
+                        new Date(req.body["dob-" + id]).getFullYear(),
+                },
+
+                location: {
+                    country: req.body["country-" + id],
+                },
+
                 phone: req.body["phone-" + id],
                 email: req.body["email-" + id],
-                picture: req.body["picture-" + id],
+                picture: {
+                    large: req.body["picture-" + id],
+                },
             };
-            console.log(userData, action);
-            res.setHeader("Content-Type", "application/json");
-            res.json(req.body);
+
+            if (action == "update") {
+                User.findByIdAndUpdate(id, userData, function (err, doc) {
+                    if (err) {
+                        console.log(err);
+                    } else {
+                        res.redirect("/");
+                    }
+                });
+            } else if (action == "remove") {
+                User.findByIdAndDelete(id, function (err, doc) {
+                    if (err) {
+                        console.log(err);
+                    } else {
+                        res.redirect("/");
+                    }
+                });
+            }
         }
+    } catch (error) {
+        res.status(500).send({ message: error.message || "Error Occurred" });
+    }
+};
 
+exports.created = async (req, res) => {
+    try {
+        const userData = {
+            name: {
+                title: req.body.title,
+                first: req.body.first,
+                last: req.body.last,
+            },
 
+            gender: req.body.gender,
+            dob: {
+                date: new Date(req.body.dob),
+                age:
+                    new Date().getFullYear() -
+                    new Date(req.body.dob).getFullYear(),
+            },
 
-        
+            location: {
+                country: req.body.country,
+            },
 
+            phone: req.body.phone,
+            email: req.body.email,
+            picture: {
+                large: req.body.picture,
+            },
+        };
 
-
-        // const users = await User.find({}).sort({_id: -1});
-
-        // console.log(users.length);
-        // res.setHeader("Content-Type", "application/json");
-        // res.json(users);
-        // users.forEach(function (user, index) {
-        //   console.log(user.dob.date, user.name.first, user.picture.thumbnail, user.email, user.phone, user.location.country);
-        //     // console.log(user.location.country)
-        // });
-
-        // res.render("index", { title: "User Profile Management - CRUD", users});
+        const newUser = User(userData);
+        newUser.save();
+        req.flash("message", "add");
+        res.redirect("/");
     } catch (error) {
         res.status(500).send({ message: error.message || "Error Occurred" });
     }
